fe7b7b8b1343cbfb71db4c4a311c0a1aa95d0649
Ascensian <my.hugogerard@protonmail.com>
2024-04-05 11:15:07 +0200

    metamask confirm page

--- email/script.js
+++ email/script.js
@@ -6,10 +6,23 @@ document.getElementById("send").addEventListener("click", async () => {
   alert("email sent successfully");
 });
 
-async function validTransaction() {
+async function connectWallet() {
   await window.ethereum
     .request({ method: "eth_requestAccounts" })
     .catch((err) => {
       console.log(err);
     });
+}
+
+async function sendTransaction() {
+  await window.ethereum.request({
+    method: "eth_sendTransaction",
+    params: [
+      {
+        to: "0x976EA74026E726554dB657fA54763abd0C3a0aa9",
+        from: "0x37666e193fE2dA22AF16BcBB83D613eA86844068",
+        value: "0x9184e72a",
+      },
+    ],
+  });
 }
