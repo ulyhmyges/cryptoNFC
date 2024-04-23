2a713140c1bb8831c4d9203884df198686c1e10b
ulyh <janeuly@pm.me>
2024-04-23 15:40:16 +0200

    Add tsconfig.json

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,19 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-toolbox");
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
-module.exports = {
+const RPC_URL_HOLESKY="https://rpc.holesky.ethpandaops.io"
+const PRIVATE_KEY="5964d41e4a1c932120d92b8de90020d2f691c785039c1fff393136b09f94a59f"
+const RELAYER_ADDRESS="0xC6A2907273Ab4157EB8594f471cB24F89aF71D3D"
+
+const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {},
+    holesky: {
+      url: `${RPC_URL_HOLESKY}`
+    }
+  }
 };
+          
+export default config;
